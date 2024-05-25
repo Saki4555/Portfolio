fbce8f3732941386c105f436a3584f8bbdefbbc8
saki4555 <saki999v@gmail.com>
2024-05-25 16:33:43 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+     <div className="bg-indigo-500 snap-y snap-mandatory scroll-smooth h-screen overflow-y-scroll text-8xl">
+      <section className="h-screen snap-start">One</section>
+      <section className="h-screen snap-start">Two</section>
+      <section className="h-screen snap-start">Three</section>
+      <section className="h-screen snap-start">Four</section>
+     </div>
+    </>
+  )
+}
+
+export default App
